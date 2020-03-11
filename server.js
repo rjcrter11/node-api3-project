d83afaa9f3858dc8c0a82f145788d8a6712863cf
rjcrter11 <rjcrter11@gmail.com>
2020-03-11 15:24:15 -0500

    initial commit, server running, logger setup

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(logger);
+server.use(express.json());
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+  console.log(`${method} to ${endpoint}`);
+
+  next();
+}
 
 module.exports = server;
