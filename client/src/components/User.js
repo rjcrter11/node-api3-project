3164b1dcd1658d0adc9972c6b905dc8adfc4626a
rjcrter11 <rjcrter11@gmail.com>
2020-03-11 21:46:02 -0500

    can add and delete user comments

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const User = (props) => {
+const initialComment = {
+  text: ""
+};
+
+const User = () => {
   const [userPosts, setUserPosts] = useState([]);
   const [userPage, setUserPage] = useState([]);
+  const [addComment, setAddComment] = useState(initialComment);
 
   const { id } = useParams();
 
@@ -27,11 +32,31 @@ const User = (props) => {
       })
       .catch((err) => console.log(err));
   };
-
   useEffect(() => {
     fetchPosts();
     fetchUser();
   }, []);
+  const deletePost = (post) => {
+    axios
+      .delete(`http://localhost:5000/api/posts/${post.id}`)
+      .then((res) => {
+        console.log(res);
+        fetchPosts();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5000/api/users/${id}/posts`, addComment)
+      .then((res) => {
+        console.log(res);
+        setAddComment(initialComment);
+        fetchPosts();
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
@@ -42,9 +67,27 @@ const User = (props) => {
             <div key={post.id} className="post-card">
               <h3> {post.text} </h3>
               <p> posted by: {post.postedBy} </p>
+              <span onClick={() => deletePost(post)} className="delete">
+                {" "}
+                x
+              </span>
             </div>
           ))}
       </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="text">Comment</label>
+        <input
+          type="text"
+          id="text"
+          value={addComment.text}
+          onChange={(e) => {
+            setAddComment({
+              text: e.target.value
+            });
+          }}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
